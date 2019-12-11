fc0b9139def5a9e86927d9f2abd25febf58eb9c0
JekyllAndHyde8999 <sravanvinakota@gmail.com>
2019-12-11 19:50:36 +0530

    Added branch Sravan

--- Main/models.py
+++ Main/models.py
@@ -8,7 +8,8 @@ class Blog(models.Model):
     text = models.CharField(max_length=50000)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     Doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
-    date = models.DateTimeField()
+    date = models.DateTimeField(auto_now=True)
+    subject = models.CharField(max_length=1000)
 
     class Meta:
        ordering = ('-date',)
